052f4539007581f68d4434b16fe519a41331c143
WWWorm92 <yadanbryvko@gmail.com>
2026-07-12 14:09:16 +0300

    AutoUpdateFixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pulsemessenger.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
