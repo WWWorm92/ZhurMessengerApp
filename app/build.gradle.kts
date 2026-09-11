78ae14379c1febdae1fc31893ffa88e2c6534608
WWWorm92 <yadanbryvko@gmail.com>
2026-09-11 13:09:25 +0300

    Version fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pulsemessenger.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 71
-        versionName = "0.7.1"
+        versionCode = 80
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
